ebbb41df278a0aebc46dce75d2b857c0da992e3a
Kimutai01 <cmoraa733@gmail.com>
2025-08-17 14:19:55 +0300

    fix

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
-import User from "@/models/User"
+import User from "@/models/user";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -20,7 +20,7 @@ export const syncUserCreation = inngest.createFunction(
             imageUrl: image_url 
         }
         await connectDB();
-        await User.create(userData)
+        await User.create(userData);
     }
 )
 
